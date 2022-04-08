5092172655fe9bbeffb5c619776f664e5302f1f3
Pere Picó Muntaner <ppicom@Peres-MacBook-Pro.local>
2022-04-08 11:04:27 +0200

    seeing how 3.1.2 works

--- __tests__/index.spec.ts
+++
@@ -1,8 +0,0 @@
-import assert from "assert";
-import { describe } from "mocha";
-
-describe("Tests named spec.ts", function () {
-  it("should run and pass", () => {
-    assert.equal(1, 1);
-  });
-});
